1f07ea328ecd814f8623b5cead2ca0da33812d37
BarWieleba <barwieleba@gmail.com>
2020-11-07 22:12:49 +0100

    adjusted pom.xml for frontend

--- frontend/src/main/java/front/example/App.java
+++ frontend/src/main/java/front/example/App.java
@@ -1,4 +1,4 @@
-package com.example;
+package front.example;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -16,7 +16,7 @@ public class App extends Application {
     public void start(Stage stage) {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
-
+        stage.setTitle("Hello");
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         var scene = new Scene(new StackPane(label), 640, 480);
         stage.setScene(scene);
